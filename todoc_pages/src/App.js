e3e58fa593c504f2aa0fc26a4f83630637e65747
salmon99 <namhj99@naver.com>
2024-06-13 14:34:58 +0900

    feat : 빈 페이지 생성

--- todoc_pages/src/App.js
+++ todoc_pages/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import WritingPage from './pages/WritingPage';
+import MainPage from './pages/MainPage';
+import SearchPage from './pages/SearchPage';
+import SignUpPage from './pages/SignUpPage';
+import MyPage from './pages/MyPages/MyPage';
+import PaymentInfoPage from './pages/MyPages/PaymentInfoPage';
+import UpdateInfoPage from './pages/MyPages/UpdateInfoPage';
+import LoginPage from './pages/LoginPages/LoginPage';
+import ForgotPasswordPage from './pages/LoginPages/ForgotPasswordPage';
+import ChangePasswordPage from './pages/LoginPages/ChangePasswordPage';
+import PaymentPage from './pages/PaymentPages/PaymentPage';
+import MembershipCancelPage from './pages/PaymentPages/MembershipCancelPage';
+import PaymentHistoryPage from './pages/PaymentPages/PaymentHistoryPage';
+import PaymentMethodPage from './pages/PaymentPages/PaymentMethodPage';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Switch>
+          <Route path="/writing" component={WritingPage} />
+          <Route path="/main" component={MainPage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/mypage" component={MyPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/payment" component={PaymentPage} />
+          <Route exact path="/" component={LandingPage} />
+          {/* Sub Pages */}
+          <Route path="/mypage/update-info" component={UpdateInfoPage} />
+          <Route path="/mypage/payment-info" component={PaymentInfoPage} />
+          <Route path="/mypage/payment-history" component={PaymentHistoryPage} />
+          <Route path="/mypage/payment-method" component={PaymentMethodPage} />
+          <Route path="/mypage/membership-cancel" component={MembershipCancelPage} />
+          <Route path="/forgot-password" component={ForgotPasswordPage} />
+          <Route path="/change-password" component={ChangePasswordPage} />
+        </Switch>
+      </Router>
   );
 }
 
