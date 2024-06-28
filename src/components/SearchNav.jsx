7c4ffae7f06e9f17a0b721b369c6c99a535d9502
yihimin <yihimin01@gmail.com>
2024-06-28 15:53:45 +0900

    feat: 서치바

--- src/components/SearchNav.jsx
+++ src/components/SearchNav.jsx
@@ -4,33 +4,27 @@ import SearchIcon from '../assets/search_icon.svg';
 import ProfileIcon from '../assets/profile_icon.svg';
 
 const SearchNav = () => {
-  const [isProfileHovered, setIsProfileHovered] = useState(false);
+  const [isProfileClicked, setIsProfileClicked] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsProfileHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsProfileHovered(false);
+  const handleProfileClick = () => {
+    setIsProfileClicked(!isProfileClicked);
   };
 
   return (
-    <div>
-      <div className="header flex justify-between items-center p-4 bg-white shadow-md">
+    <div className="PilsaPage">
+      <div className="header">
         <div className="logo">
-          <img src={TodocLogo} alt="Todoc Logo" />
+          <img src={TodocLogo} alt="Todoc Logo"/>
         </div>
-        <div className="search-container relative flex-grow mx-4">
-          <div className="search-bar flex items-center">
-            <input type="text" className="search-input flex-grow py-2 px-4 border rounded-l-md" placeholder="검색어를 입력하세요" />
-            <span className="search-icon bg-gray-200 p-2 rounded-r-md">
-              <img src={SearchIcon} alt="Search" />
-            </span>
+        <div className="search-container">
+          <div className="search-bar">
+            <input type="text" className="search-input" placeholder="검색어를 입력하세요"/>
+            <span className="search-icon"><img src={SearchIcon} alt="Search"/></span>
           </div>
         </div>
-        <div className="user-icon relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-          <img src={ProfileIcon} alt="Profile" className="cursor-pointer" />
-          {isProfileHovered && (
+        <div className="user-icon relative">
+          <img src={ProfileIcon} alt="Profile" className="cursor-pointer" onClick={handleProfileClick} />
+          {isProfileClicked && (
             <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
               <ul>
                 <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">마이페이지</li>
