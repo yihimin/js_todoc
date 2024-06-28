bcdfbf8ee3b36d6511a96415da9720b4f02af623
yihimin <yihimin01@gmail.com>
2024-06-28 14:11:40 +0900

    feat: 회원가입/로그인

--- src/components/SignupNav.jsx
+++ src/components/SignupNav.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import TodocLogo from '../assets/todoc_logo.svg';
 
 const SignUpNav = () => {
   return (
     <div className="w-full h-[72px] px-8 py-4 bg-stone-50 shadow flex justify-between items-center">
       <div className="flex items-center">
-        <img src={TodocLogo} alt="Todoc Logo" className="h-8 mr-4"/>
+        <Link to="/main">
+          <img src={TodocLogo} alt="Todoc Logo" className="h-8 mr-4"/>
+        </Link>
       </div>
       <div className="flex items-center">
-        <button className="w-[91px] h-10 px-4 py-2 bg-white text-stone-500 border border-stone-500 rounded mr-2">로그인</button>
-        <button className="w-[91px] h-10 px-4 py-2 bg-customGreen text-white rounded">회원가입</button>
+        <Link to="/login">
+          <button className="w-[91px] h-10 px-4 py-2 bg-white text-stone-500 border border-stone-500 rounded mr-2">
+            로그인
+          </button>
+        </Link>
+        <Link to="/signup">
+          <button className="w-[91px] h-10 px-4 py-2 bg-customGreen text-white rounded">
+            회원가입
+          </button>
+        </Link>
       </div>
     </div>
   );
