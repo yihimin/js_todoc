8bea8711b28d2798785a538bad814cd5cc5cde98
yihimin <yihimin01@gmail.com>
2024-06-28 15:25:13 +0900

    feat: 컴포넌트 네브바

--- src/components/SearchNav.jsx
+++ src/components/SearchNav.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodocLogo from '../assets/todoc_logo.svg';
 import SearchIcon from '../assets/search_icon.svg';
 import ProfileIcon from '../assets/profile_icon.svg';
 
 const SearchNav = () => {
+  const [isProfileHovered, setIsProfileHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsProfileHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsProfileHovered(false);
+  };
+
   return (
-    <div className="PilsaPage">
-            <div className="header">
-                <div className="logo">
-                    <img src={TodocLogo} alt="Todoc Logo"/>
-                </div>
-                <div className="search-container">
-                    <div className="search-bar">
-                        <input type="text" className="search-input" placeholder="검색어를 입력하세요"/>
-                        <span className="search-icon"><img src={SearchIcon} alt="Search"/></span>
-                    </div>
-                </div>
-                <div className="user-icon"><img src={ProfileIcon} alt="Profile"/></div>
+    <div>
+      <div className="header flex justify-between items-center p-4 bg-white shadow-md">
+        <div className="logo">
+          <img src={TodocLogo} alt="Todoc Logo" />
+        </div>
+        <div className="search-container relative flex-grow mx-4">
+          <div className="search-bar flex items-center">
+            <input type="text" className="search-input flex-grow py-2 px-4 border rounded-l-md" placeholder="검색어를 입력하세요" />
+            <span className="search-icon bg-gray-200 p-2 rounded-r-md">
+              <img src={SearchIcon} alt="Search" />
+            </span>
+          </div>
+        </div>
+        <div className="user-icon relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={ProfileIcon} alt="Profile" className="cursor-pointer" />
+          {isProfileHovered && (
+            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
+              <ul>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">마이페이지</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">진행중 필사</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">완료된 필사</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">스크랩</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">메모한 글</li>
+              </ul>
             </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
