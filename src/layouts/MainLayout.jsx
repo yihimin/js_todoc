7dc155a9a0d6f81988ed0f22feaa4a15b998d70c
himin <yihimin01@gmail.com>
2024-08-01 01:09:21 +0900

    fix: 아까꺼

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
-import SearchNav from "../components/SearchNav";
 
 const MainLayout = () => {
   return (
