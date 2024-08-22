15dcac30418b7a4ade351d7dc83ab733d1016295
salmon99 <namhj99@naver.com>
2024-08-23 03:00:24 +0900

    fix : landing page 수정

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 import SearchNav from '../components/SearchNav';
 import PilsaComponent from '../components/PilsaComponent';
 import LandingBackground from "../assets/landing_background.svg";
-import PileOfBooks from "../assets/pile_of_books.png";
 
 const LandingPage = () => {
     return (
         <div>
-            <SearchNav/>
             <div className="flex flex-col h-screen">
-                <div className="relative" style={{paddingTop: '64.24%'}}>
+                <div className="relative" style={{paddingTop: '72.22%'}}>
                     <img
                         src={LandingBackground}
                         alt="LandingBackground"
-                        className="absolute z-20 mt-[30.12px] top-0 left-0 w-full h-full"
-                        style={{ right: 'calc(12.54% + 10px)' }}/>
-                    <img
-                        src={PileOfBooks}
-                        alt="PileOfBooks"
-                        className="absolute z-30"/>
+                        className="absolute z-20 mt-[30.12px] top-0 left-0 w-full h-full"/>
                 </div>
-                <div className="mt-[1121px]">
+                <div className="mt-[700px] flex justify-center items-center">
+                    <p className="text-[56px] text-[#0f0f0f] font-black">
+                        한번 써볼까요?
+                    </p>
+                </div>
+                <div className="">
                     <PilsaComponent/>
                 </div>
             </div>
