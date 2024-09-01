1e1d70293b7fadc8fd6a0700c90517d23ccf0d0a
salmon99 <namhj99@naver.com>
2024-09-01 18:19:53 +0900

    fix : navigation bar

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import LandingBackground from "../assets/landing_background.svg";
 import PilsaComponent from '../components/PilsaComponent';
+import SignUpNav from "../components/SignupNav";
 
 const LandingPage = () => {
     return (
-        <div className="flex flex-col min-h-screen">
-            <div className="relative flex-grow" style={{paddingTop: '72.22%'}}>
-                <img
-                    src={LandingBackground}
-                    alt="LandingBackground"
-                    className="absolute z-20 mt-[30.12px] top-0 left-0 w-full h-full"
-                />
-            </div>
-            <div className="mt-[700px] flex justify-center items-center">
-                <p className="text-[56px] text-[#0f0f0f] font-black">
-                    한번 써볼까요?
-                </p>
-            </div>
-            <div className="flex-grow">
-                <PilsaComponent/>
-            </div>
+        <div>
+            <SignUpNav/>
+                <div className="flex flex-col min-h-screen snap-y">
+                    <div className="relative flex-grow" style={{paddingTop: '72.22%'}}>
+                        <img
+                            src={LandingBackground}
+                            alt="LandingBackground"
+                            className="absolute z-20 mt-[30.12px] top-0 left-0 w-full h-full"
+                        />
+                    </div>
+                    <div className="mt-[700px] flex justify-center items-center">
+                        <p className="text-[56px] text-[#0f0f0f] font-black">
+                            한번 써볼까요?
+                        </p>
+                    </div>
+                    <div className="flex-grow">
+                        <PilsaComponent/>
+                    </div>
+                </div>
         </div>
     );
 };
