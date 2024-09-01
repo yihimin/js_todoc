9af26bda4e031f37dcc367678ea395060d3ef6ca
salmon99 <namhj99@naver.com>
2024-09-01 20:47:45 +0900

    fix : 쫀득한 스크롤 수정중

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,17 +5,17 @@ import SignUpNav from "../components/SignupNav";
 
 const LandingPage = () => {
     return (
-        <div>
-            <SignUpNav/>
-                <div className="flex flex-col min-h-screen snap-y">
-                    <div className="relative flex-grow" style={{paddingTop: '72.22%'}}>
+        <div className="h-screen overflow-scroll snap-y snap-mandatory">
+                <div className="flex flex-col">
+                    <SignUpNav/>
+                    <div className="relative flex-grow snap-center" style={{paddingTop: '72.22%'}}>
                         <img
                             src={LandingBackground}
                             alt="LandingBackground"
                             className="absolute z-20 mt-[30.12px] top-0 left-0 w-full h-full"
                         />
                     </div>
-                    <div className="mt-[700px] flex justify-center items-center">
+                    <div className="mt-[700px] flex justify-center items-center snap-center">
                         <p className="text-[56px] text-[#0f0f0f] font-black">
                             한번 써볼까요?
                         </p>
