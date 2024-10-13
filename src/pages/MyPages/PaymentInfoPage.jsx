6c0c343f3c440c799394010b34e39f8fd99e3f64
salmon99 <namhj99@naver.com>
2024-10-14 00:44:12 +0900

    fix : PaymentInfoPage 수정

--- src/pages/MyPages/PaymentInfoPage.jsx
+++ src/pages/MyPages/PaymentInfoPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import SearchNav from "../../components/SearchNav";
 import Modal from '../../modal/Modal';
+import ExpandRight from "../../assets/expand_right.svg"
 
 const PaymentInfoPage = () => {
   const navigate = useNavigate();
@@ -36,29 +37,34 @@ const PaymentInfoPage = () => {
   return (
     <>
       <SearchNav />
-      <div className="flex justify-center items-center h-screen">
-        <div className="bg-white p-10 rounded-lg shadow w-full max-w-md text-center">
-          <h2 className="text-xl font-bold mb-6">
+      <div className="flex justify-center h-screen">
+        <div className="bg-white rounded-lg flex flex-col justify-center">
+          <div className="flex flex-col items-center">
+          <h2 className="text-2xl font-bold mb-[37px] whitespace-nowrap">
             다음 결제일은{" "}
-            <span className="text-customGreen">2024년 03월 26일</span>이에요.
+            <span className="text-customGreen">2024년 03월 26일</span> 이에요.
           </h2>
-          <div
-            className="py-2 px-4 border rounded text-gray-700 flex justify-between items-center hover:bg-gray-200 cursor-pointer"
-            onClick={handlePaymentHistory}
-          >
-            <span>결제 내역 확인하기</span>
-            <span className="text-gray-400">{">"}</span>
+          <div className="w-[374px] h-[116px] rounded-[10px] border border-[#9b9b9b]">
+            <div
+                className="py-[15.5px] pl-[20px] text-[15px] text-[#8a8a8a] flex justify-between items-center hover:text-customGreen cursor-pointer"
+                onClick={handlePaymentHistory}>
+              <span>결제 내역 확인하기</span>
+              <img className="pr-[16px]"
+                   src={ExpandRight}/>
+            </div>
+            <div className="mx-2 w-[356px] h-[0px] border border-[#b0b0b0]"></div>
+            <div
+                className="py-[15.5px] pl-[20px] text-[15px] text-[#8a8a8a] flex justify-between items-center hover:text-customGreen cursor-pointer"
+                onClick={handlePaymentMethod}>
+              <span>결제 수단 변경</span>
+              <img className="pr-[16px]"
+                   src={ExpandRight}/>
+            </div>
           </div>
-          <div
-            className="mb-4 py-2 px-4 border rounded text-gray-700 flex justify-between items-center hover:bg-gray-200 cursor-pointer"
-            onClick={handlePaymentMethod}
-          >
-            <span>결제 수단 변경</span>
-            <span className="text-gray-400">{">"}</span>
           </div>
           <div className="flex justify-end">
             <button
-              className="text-gray-500 hover:text-red-500 focus:outline-none underline"
+              className="mt-[22px] mr-[29px] text-[#b0b0b0] text-[15px] hover:text-red-500 focus:outline-none underline"
               onClick={openModal}>
               멤버쉽 해지하기
             </button>
